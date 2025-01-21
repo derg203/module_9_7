8375319a883e839688a970865fb324b3921d78e2
derg203 <ddergacev714@gmail.com>
2025-01-21 16:59:42 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,20 @@
+def is_prime(func):
+    def wrapper(*args, ** kwargs):
+        result = func(*args, ** kwargs)
+        summa = sum(args)
+        a = 0
+        for i in range(2, summa // 2 + 1):
+            if summa % i == 0:
+                a = a +1
+        if a <= 0:
+            print('Простое')
+        else:
+            print('Составное')
+        return result
+    return wrapper
+@is_prime
+def sum_three(*args):
+    return sum(args)
+
+result = sum_three(2, 3, 6)
+print(result)
